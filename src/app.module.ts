00dc5902706870cac7e14136f54150591ff8af67
cyvac1 <cyvac1@jinhakapply.com>
2023-01-09 12:04:47 +0900

    feat: users 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
       synchronize: true,
       namingStrategy: new SnakeNamingStrategy(),
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
