d9e97e70acdfe6c2216b50a205afe881151665f0
cyvac1 <cyvac1@jinhakapply.com>
2023-01-09 11:22:40 +0900

    feat: typeORM 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '0000',
+      database: 'INNERVIEWON',
+      schema: 'INNERVIEWON',
+      entities: [],
+      synchronize: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
