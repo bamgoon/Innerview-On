6f51401b190b25c815c9cf3fe60262dfae585aa6
cyvac1 <cyvac1@jinhakapply.com>
2023-01-09 14:09:24 +0900

    feat: users REST API 설정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Get,
+  Patch,
+  Post,
+  Delete,
+  Body,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  signUp(@Body() signUpData) {
+    return this.usersService.signUp(signUpData);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') uid: number) {
+    return this.usersService.getOne(uid);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') uid: number, @Body() updateData) {
+    return this.usersService.update(uid, updateData);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') uid: number) {
+    return this.usersService.delete(uid);
+  }
+
+  @Post('/signin')
+  signIn(@Body() signInData) {
+    return this.usersService.signIn(signInData);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  signUp(signUpData) {
+    return `${signUpData}로 회원가입 진행`;
+  }
+  signIn(signInData) {
+    return `${signInData}로 로그인 진행`;
+  }
+  getAll() {
+    return `모든 유저 조회`;
+  }
+  getOne(id: number) {
+    return `특정 유저 조회`;
+  }
+  update(id: number, updateData) {
+    return `${updateData}로 특정 유저 수정`;
+  }
+  delete(id: number) {
+    return `특정 유저 삭제`;
+  }
+}
