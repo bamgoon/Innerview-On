834729330020620dce31c72160236ceefe620f27
cyvac1 <cyvac1@jinhakapply.com>
2023-01-18 11:37:59 +0900

    feat: rooms REST API 설정

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { RoomsService } from './rooms.service';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+  constructor(private readonly roomsService: RoomsService) {}
+
+  @Post()
+  create(@Body() createRoomDto) {
+    return this.roomsService.create(createRoomDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.roomsService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number) {
+    return this.roomsService.getOne(id);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: number, @Body() updateRoomDto) {
+    return this.roomsService.update(id, updateRoomDto);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: number) {
+    return this.roomsService.delete(id);
+  }
+
+  @Get('/enter/:id')
+  enter(@Param('id') id: number) {
+    return this.roomsService.enter(id);
+  }
+}
--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+  create(createRoomDto) {
+    return `${createRoomDto}방 생성`;
+  }
+  getAll() {
+    return `모든 방 조회`;
+  }
+  getOne(id: number) {
+    return `${id}} 방 조회`;
+  }
+  update(id: number, updateRoomDto) {
+    return `${id} 방 수정, ${updateRoomDto}`;
+  }
+  delete(id: number) {
+    return `${id} 방 삭제`;
+  }
+  enter(id: number) {
+    return `${id} 방 입장`;
+  }
+}
