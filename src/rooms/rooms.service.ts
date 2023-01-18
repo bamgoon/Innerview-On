d2a19ddb7a7468185bcaae4ce8266f713724572a
cyvac1 <cyvac1@jinhakapply.com>
2023-01-18 17:08:08 +0900

    feat: rooms 서비스, 엔티티 연결

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,21 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Room } from './entities/room.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class RoomsService {
-  create(createRoomDto) {
-    return `${createRoomDto}방 생성`;
+  async create(createRoomDto) {
+    const { mainTitle, subTitle, openDate, closeDate, uids } = createRoomDto;
+    const room = new Room();
+    room.mainTitle = mainTitle;
+    room.subTitle = subTitle;
+    room.openDate = openDate;
+    room.closeDate = closeDate;
+    room.users = [];
+    for (const id of uids) room.users.push(await User.findOneBy({ id }));
+
+    await room.save();
   }
-  getAll() {
-    return `모든 방 조회`;
+  async getAll() {
+    return await Room.createQueryBuilder('room')
+      .leftJoin('room.users', 'user')
+      .select([
+        'room.mainTitle',
+        'room.subTitle',
+        'user.name',
+        'user.role',
+        'user.email',
+        'user.phoneNumber',
+      ])
+      .getMany();
   }
-  getOne(id: number) {
-    return `${id}} 방 조회`;
+  async getOne(id: number) {
+    return await Room.createQueryBuilder('room')
+      .where('room.id = :id', { id })
+      .leftJoin('room.users', 'user')
+      .select([
+        'room.mainTitle',
+        'room.subTitle',
+        'user.name',
+        'user.role',
+        'user.email',
+        'user.phoneNumber',
+      ])
+      .getOne();
   }
   update(id: number, updateRoomDto) {
     return `${id} 방 수정, ${updateRoomDto}`;
   }
-  delete(id: number) {
-    return `${id} 방 삭제`;
+  async delete(id: number) {
+    return await Room.delete({ id });
   }
   enter(id: number) {
     return `${id} 방 입장`;
